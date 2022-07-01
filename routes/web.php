f87aa7eda56ccb3dce6b783c08ed3f2f9f2e240b
DThanh1508 <athanh2002kt@gmail.com>
2022-07-01 08:21:14 +0700

    upload code again

--- routes/web.php
+++ routes/web.php
@@ -28,32 +28,3 @@ Route::put('cars/{car}',[CarController::class,'update'])->name('cars.update');
 Route::get('cars/{car}',[CarController::class,'show'])->name('cars.show');
 Route::delete('cars/{car}/delete',[CarController::class,'destroy'])->name('cars.destroy');
 // Route::get('/cars/delete/{car}', [CarController::class, 'destroy'])->name('cars.delete');
-Route::get('ptb1',function(){
-    return view('ptb1');
-});
-// Route::get('ptb1_c2',function(){
-//     return view('ptb1_c2');
-// });
-// Route::get('caculator',function(){
-//     return view('caculator');
-// });
-// Route::get('test',function(){
-//     return view('test');
-// });
-Route::post('ptb1', function (Request $req) {
-    $a = $req->input('a');
-    $b = $req->input('b');
-    if($a==0)
-    if($b==0)
-    $kq = "PT có V số N";
-    else $kq ="PTVN";
-    else $kq ="PTCN x=".round(-$b/$a,2);
-    return view('ptb1',compact('a','b','kq'));
-})->name('ptb1.post');
-Route::post('ptb1',[postptb1::class,'postptb1'])->name('ptb1.post');
-Route::get('ptb1_c2',[getptb1::class,'getptb1'])->name('ptb1.get');
-// Route::post('caculator',[caculator::class,'caculator'])->name('caculator.post');
-// Route::get('tinh2',function(){
-//     return view('tinh2');
-// });
-// Route::post('tinh2',[tinh::class,'tinhLop1'])->name('xoi');
